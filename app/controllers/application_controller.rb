9d65959757a6dda7518d2f8d53dfae7a96a3361b
Arvand Alviri <arvando@Arvands-MacBook-Pro.local>
2015-03-23 13:31:50 -0600

    admin, expert, separated out in home view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,27 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+
+  def logged_in?
+  	current_user.present?
+  end
+
+  #used as before filters in controller so it locks out entire controller so people cant access stuff
+  def ensure_current_user
+  	if !logged_in?
+		flash[:error] = "You must be signed in"
+		redirect_to root_path and return
+	end
+  end
+
+  def ensure_expert
+  	raise ActionController::RoutingError.new('Not Found') unless logged_in? && current_user.expert?
+  end
+
+  def ensure_admin
+  	raise ActionController::RoutingError.new('Not Found') unless logged_in? && current_user.admin?
+  end
+
+
+  helper_method :current_user, :logged_in?
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	has_many :declines
 	has_many :bookings
 
+  def self.expert; where('expert IS TRUE'); end #couldn't use this in home view as ...expert? because this just returns a list of experts and you cant use current_user in it.
+
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
