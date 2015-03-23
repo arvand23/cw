d67b7204b5defdfb90351dfdc265762b462b1d11
Arvand Alviri <arvando@Arvands-MacBook-Pro.local>
2015-03-23 16:22:04 -0600

    fixed expert bug

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
   def home
     @pending_experts = User.pending_expert
+    @pending_experts_nil = User.pending_expert_nil #because need to make boolean default => false
     @experts = User.expert
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ActiveRecord::Base
 
   def self.expert; where(:expert => TRUE); end #couldn't use this in home view as ...expert? because this just returns a list of experts and you cant use current_user in it.
   def self.applied_as_expert; where(:pending_expert => TRUE); end
-  def self.notexpert; where(:expert => FALSE); end
+  def self.nilexpert; where(:expert => nil); end
+  def self.notexpert; where(:expert => false); end
   def self.pending_expert; applied_as_expert.notexpert; end
+  def self.pending_expert_nil; applied_as_expert.nilexpert; end #because need to make boolean default => false
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
