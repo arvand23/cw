948b97476d472dfea9ae591742ce93521caac89a
Arvand Alviri <arvando@Arvands-MacBook-Pro.local>
2015-03-23 16:05:03 -0600

    fixed bug about experts vs pending experts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323200901) do
+ActiveRecord::Schema.define(version: 20150323215051) do
 
   create_table "bookings", force: true do |t|
     t.string   "skill"
