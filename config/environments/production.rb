1182a7bcb9e3b60b38f480967e31befafb9cbf76
Arvand Alviri <arvando@Arvands-MacBook-Pro.local>
2015-03-26 15:58:31 -0600

    production.rb change for heroku mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Cw2::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'https://arcane-ravine-1260.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
